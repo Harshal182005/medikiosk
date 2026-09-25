c3332795df259f0044428426b16ed8e56b6d03cc
Harshal182005 <pawarharshal205@gmail.com>
2026-09-25 16:20:33 +0530

    Connect frontend to deployed backend

--- client/src/pages/DoctorLogin.jsx
+++ client/src/pages/DoctorLogin.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 function DoctorLogin() {
     const navigate = useNavigate();
